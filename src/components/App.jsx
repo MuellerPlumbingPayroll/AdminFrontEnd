72030d9bf0323e5e6434f8a1b3c5184402a634a6
Tanner Brunscheon <tannerbrunscheon@gmail.com>
2019-02-12 18:21:38 -0600

    Made Sidebar not Be awful. Base on https://github.com/primefaces/sigma

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,10 @@
 /* eslint-disable react/destructuring-assignment */
 import React from 'react';
+import classNames from 'classnames';
 import { Sidebar } from 'primereact/sidebar';
 import { Button } from 'primereact/button';
-import '../stylesheets/App.css';
+import SidebarContent from './partials/SideBarContent';
+import '../stylesheets/vars.scss';
 import 'primereact/resources/themes/nova-light/theme.css';
 import 'primereact/resources/primereact.min.css';
 import 'primeicons/primeicons.css';
@@ -13,25 +15,38 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      visible: false,
+      layoutMode: 'static',
+      layoutColorMode: 'dark',
+      mobileMenuActive: false,
     };
   }
 
+  isDesktop = () => {
+    return window.innerWidth > 1024;
+  }
+
+  componentDidUpdate = ()=> {
+    if (this.state.mobileMenuActive) { this.addClass(document.body, 'body-overflow-hidden'); } else { this.removeClass(document.body, 'body-overflow-hidden'); }
+  }
 
   render() {
+    const listOf = [
+      { label: 'Manage Employees', icon: 'pi pi-fw pi-users' },
+      { label: 'Manage Jobs', icon: 'pi pi-fw pi-home' },
+      { label: 'Manage Job Codes', icon: 'pi pi-fw pi-key' },
+      { label: 'Download Time Sheets', icon: 'pi pi-fw pi-calendar' },
+    ];
+  
+    const wrapperClass = classNames('layout-wrapper', {
+      'layout-static': this.state.layoutMode === 'static',
+      'layout-mobile-sidebar-active': this.state.mobileMenuActive,
+    });
+    const sidebarClassName = classNames('layout-sidebar', { 'layout-sidebar-dark': this.state.layoutColorMode === 'dark' });
     return (
-      <div>
-        <Sidebar
-          position="left"
-          visible={this.state.visible}
-          onHide={e => this.setState({ visible: false })}
-        >
-          Test
-        </Sidebar>
-        <Button
-          icon="pi pi-arrow-right"
-          onClick={e => this.setState({ visible: true })}
-        />
+      <div className={wrapperClass}>
+        <div className={sidebarClassName}>
+          <SidebarContent model={listOf}/>
+        </div>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 /* eslint-disable react/jsx-filename-extension */
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './stylesheets/index.css';
+import './stylesheets/vars.scss';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 
